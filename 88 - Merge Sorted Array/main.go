96f7b8bbc5ffb6dbb33d8c77911b7ea4d6425591
Richard Liu <richardliux23@gmail.com>
2023-10-08 14:39:43 +0800

    Sample answer

--- 88 - Merge Sorted Array/main.go
+++ 88 - Merge Sorted Array/main.go
@@ -2,18 +2,21 @@ package main
 
 import (
 	"fmt"
-	"sort"
 )
 
 func merge(nums1 []int, m int, nums2 []int, n int) {
-	var newArr []int
-	for i := 0; i < m; i++ {
-		newArr = append(newArr, nums1[i])
+	for p := m + n; m > 0 && n > 0; p-- {
+		if nums1[m-1] <= nums2[n-1] {
+			nums1[p-1] = nums2[n-1]
+			n--
+		} else {
+			nums1[p-1] = nums1[m-1]
+			m--
+		}
 	}
-	for j := 0; j < n; j++ {
-		newArr = append(newArr, nums2[j])
+	for ; n > 0; n-- {
+		nums1[n-1] = nums2[n-1]
 	}
-	sort.Ints(newArr)
 }
 
 func main() {
