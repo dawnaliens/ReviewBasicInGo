3cf2afe6906f5624048b0911590d50a57ddfb727
Richard Liu <richardliux23@gmail.com>
2023-10-09 13:34:45 +0800

    Ugly Number

--- 263 - Ugly Number/main.go
+++ 263 - Ugly Number/main.go
@@ -2,21 +2,28 @@ package main
 
 import "fmt"
 
+var factors = []int{2, 3, 5}
+
 func isUgly(n int) bool {
-	if n%2 == 0 && n%3 == 0 && n%5 == 0 {
-		return true
-	} else if n%2 == 0 || n%3 == 0 || n%5 == 0 {
-		return true
-	} else {
+	if n <= 0 {
 		return false
 	}
+	for _, f := range factors {
+		for n%f == 0 {
+			n /= f
+		}
+	}
+	return n == 1
 }
 
 func main() {
 	res1 := isUgly(6)
 	res2 := isUgly(1)
 	res3 := isUgly(14)
+	res4 := isUgly(8)
 	fmt.Println(res1)
 	fmt.Println(res2)
 	fmt.Println(res3)
+	fmt.Println(res4)
+
 }
