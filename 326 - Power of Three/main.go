81ae721dc397934d2d8c8c056c3053e5c71591e1
Richard Liu <richardliux23@gmail.com>
2023-10-09 13:14:31 +0800

    Ugly Num

--- 326 - Power of Three/main.go
+++ 326 - Power of Three/main.go
@@ -5,27 +5,36 @@ import (
 	"math"
 )
 
+//func isPowerOfThree(n int) bool {
+//	if n <= 0 {
+//		return false
+//	}
+//	for i := 0; i < n; i++ {
+//		if math.Pow(float64(3), float64(i)) == float64(n) {
+//			return true
+//		}
+//	}
+//	return false
+//}
+
+// Official solution
 func isPowerOfThree(n int) bool {
-	if n <= 0 {
-		return false
+	for n > 0 && n%3 == 0 {
+		n = n / 3
 	}
-	for i := 0; i < n; i++ {
-		if math.Pow(float64(3), float64(i)) == float64(n) {
-			return true
-		}
-	}
-	return false
+	return n == 1
 }
 
 func main() {
-	num1 := 27
+	num1 := 14348906
 	res := math.Pow(float64(3), float64(4))
 	fmt.Println(res)
-
-	for i := 0; i < num1; i++ {
-		if math.Pow(float64(3), float64(i)) == float64(num1) {
-			fmt.Println(float64(i))
-			fmt.Println(true)
-		}
-	}
+	res1 := isPowerOfThree(num1)
+	fmt.Println(res1)
+	//for i := 0; i < num1; i++ {
+	//	if math.Pow(float64(3), float64(i)) == float64(num1) {
+	//		fmt.Println(float64(i))
+	//		fmt.Println(true)
+	//	}
+	//}
 }
