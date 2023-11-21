130fd509e3a4afae1afcdf507ddc076cbd0ea88d
Richard Liu <richardliux23@gmail.com>
2023-11-21 21:14:26 +0800

    edit

--- 389 - Find the Difference/main.go
+++ 389 - Find the Difference/main.go
@@ -1,8 +1,6 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 //func findTheDifference(s string, t string) byte {
 //
@@ -10,19 +8,9 @@ import (
 
 func main() {
 	s := "abcd"
-	t := "abcde"
-	sLen := len(s)
-	tLen := len(t)
-
-	// output := make([]byte, 0)
-	s1 := []byte(s)
-	fmt.Println(s1)
-
-	t1 := []byte(t)
-	fmt.Println(t1)
-
-	difference := make([]byte, 0)
-	for i := 0; i < sLen || i < tLen; i++ {
-		
+	//t := "abcde"
+	for i := 0; i < len(s); i++ {
+		fmt.Println(s[i])
 	}
+	
 }
