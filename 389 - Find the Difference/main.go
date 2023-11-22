a00ab6a617c47b40424c7f5dfab8b5366e585876
Richard Liu <richardliux23@gmail.com>
2023-11-22 17:00:31 +0800

    Wrong solution

--- 389 - Find the Difference/main.go
+++ 389 - Find the Difference/main.go
@@ -2,15 +2,36 @@ package main
 
 import "fmt"
 
-//func findTheDifference(s string, t string) byte {
-//
-//}
+func findTheDifference(s string, t string) byte {
+
+	minLen := len(s)
+	if len(t) < minLen {
+		minLen = len(t)
+	}
+
+	for i := 0; i < minLen; i++ {
+		if s[i] != t[i] {
+			return s[i]
+		}
+	}
+	if len(s) > len(t) {
+		return s[minLen]
+	} else if len(s) < len(t) {
+		return t[minLen]
+	}
+	return 0
+}
 
 func main() {
 	s := "abcd"
-	//t := "abcde"
-	for i := 0; i < len(s); i++ {
-		fmt.Println(s[i])
-	}
-	
+	t := "abcde"
+
+	str1 := ""
+	str2 := "y"
+
+	res1 := findTheDifference(s, t)
+	fmt.Println(res1)
+
+	res2 := findTheDifference(str1, str2)
+	fmt.Println(res2)
 }
